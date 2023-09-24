5071e1ccb3b6b23ca175d1bbc81ca8f85f5ce2ec
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-23 21:45:06 -0300

    javaScript feito

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const paineis = document.querySelectorAll('.painel');
+
+paineis.forEach((painel) => {
+    painel.addEventListener('click', () => {
+        removeActiveClass()
+        painel.classList.add('active')
+    })
+})
+function removeActiveClass(){
+    paineis.forEach(painel => {
+        painel.classList.remove('active')
+    })
+}
