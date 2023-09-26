a9896608552935c6a2ec852eccc794438e7f4cd1
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-26 19:42:05 -0300

    tela de loading

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,45 @@
+const personagens = [
+    {
+        nome: 'usopp',
+        força: Math.floor(Math.random()*  80 + 30) ,
+        haki: Math.floor(Math.random()*  150 + 30) ,
+        agilidade:Math.floor(Math.random()* 40 + 80) , 
+    },
+    {
+        nome: 'Luffy',
+        força: Math.floor(Math.random()*  40 + 10) ,
+        haki: Math.floor(Math.random()*  100 + 30) ,
+        agilidade:Math.floor(Math.random()* 60 + 40) , 
+    },
+    {
+        nome: 'nami',
+        força: Math.floor(Math.random()*  40 + 55) ,
+        haki: Math.floor(Math.random()*  80 + 50) ,
+        agilidade:Math.floor(Math.random()* 40 + 50) , 
+    }
+];
+
+const sortear = document.getElementById('sortear')
+const primeiraTela = document.getElementById('firstScreen')
+const segundaTela = document.getElementById('secondScreen')
+const telaAposta = document.getElementById('telaAposta')
+const terceiraTela = document.getElementById('thirdSecond')
+
+document.querySelector('#sortear').addEventListener('click', function(){
+    primeiraTela.style.display = 'none';
+    segundaTela.style.display = 'flex'
+
+}) 
+
+
+
+
+
+
+
 const paineis = document.querySelectorAll('.painel');
+
+
 
 paineis.forEach((painel) => {
     painel.addEventListener('click', () => {
@@ -11,3 +52,4 @@ function removeActiveClass(){
         painel.classList.remove('active')
     })
 }
+
